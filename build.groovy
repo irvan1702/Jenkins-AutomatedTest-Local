3691ad8292462e0c3e13ccc897350d079aa99f69
irvan1702 <irvanfebrianto94@gmail.com>
2018-02-05 17:36:19 +0700

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,7 +1,7 @@
 node('master'){
 
         stage('download source code'){
-                git clone https://$USERNAME:$PASSWORD@github.com/irvan1702/Jenkins-AutomatedTest-Local.git
+                bat "git clone https://$USERNAME:$PASSWORD@github.com/irvan1702/Jenkins-AutomatedTest-Local.git"
         }
         
         stage('build artifact'){
