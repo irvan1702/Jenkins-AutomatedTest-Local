58254cff9101aa63e497b2c6f9ce28fbc5f06a48
irvan1702 <irvanfebrianto94@gmail.com>
2018-02-05 17:22:38 +0700

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,7 +1,7 @@
 node('master'){
 
         stage('build artifact'){
-               cd "C:\17054012\Data\List Project FES\Testing\webdriver"
+               cd "C:/17054012/Data/List Project FES/Testing/webdriver"
                bat "mvn clean install";
         }
 }
